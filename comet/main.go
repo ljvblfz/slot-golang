c70b38b50f5825ac1d2117ca4d20d02c5c2bdcb9
steven <steven.cuixin@gmail.com>
2014-06-12 15:41:27 +0800

    add some flags on comet.

--- comet/main.go
+++ comet/main.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"flag"
-	"log"
 	"github.com/golang/glog"
+	"strings"
 )
 
 var (
-	gSessionList *SessionList
+	gSessionList  *SessionList
+	gMsgBusServer *MsgBusServer
 )
 
 func main() {
 	rh := flag.String("rh", "193.168.1.224:6379", "Redis地址")
+	msgbusAddr := flag.String("msgbus", "localhost:9923", "MsgBus地址")
+	lHost := flag.String("ports", ":1234,:1235", "监听的websocket地址")
 	flag.Parse()
-
-	gSessionList = InitSessionList()
 	initRedix(*rh)
 
-	StartHttp([]string{":1234", ":1235"})
+	gMsgBusServer = NewMsgBusServer(*msgbusAddr)
+	if gMsgBusServer.Dail() != nil {
+		return
+	}
+	go gMsgBusServer.Reciver()
+
+	gSessionList = InitSessionList()
+	hostList := strings.Split(*lHost, ",")
+	StartHttp(hostList)
 	glog.Infoln("Server starting...")
+
 	handleSignal(func() {
 		glog.Info("Closed Server")
-		log.Println("Closed Server")
 	})
 }
