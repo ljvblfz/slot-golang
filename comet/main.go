65dadf524acdb18dfa1ca72b1b0242dcd9fafaae
steven <steven.cuixin@gmail.com>
2014-06-13 14:51:03 +0800

    add's comet's zk initialization.

--- comet/main.go
+++ comet/main.go
@@ -13,16 +13,13 @@ var (
 
 func main() {
 	rh := flag.String("rh", "193.168.1.224:6379", "Redis地址")
-	msgbusAddr := flag.String("msgbus", "localhost:9923", "MsgBus地址")
+	// msgbusAddr := flag.String("msgbus", "localhost:9923", "MsgBus地址")
 	lHost := flag.String("ports", ":1234,:1235", "监听的websocket地址")
 	flag.Parse()
 	initRedix(*rh)
-
-	gMsgBusServer = NewMsgBusServer(*msgbusAddr)
-	if gMsgBusServer.Dail() != nil {
-		return
+	if err := InitZK([]string{"193.168.1.221", "193.168.1.222", "193.168.1.223"}); err != nil {
+		glog.Fatal(err)
 	}
-	go gMsgBusServer.Reciver()
 
 	gSessionList = InitSessionList()
 	hostList := strings.Split(*lHost, ",")
