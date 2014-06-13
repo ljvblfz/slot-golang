fd31eb4893c0f21c5dcef3b443ebf855b37e8d48
steven <steven.cuixin@gmail.com>
2014-06-13 14:49:39 +0800

    add msgbus's zk.

---
+++ msgbus/zk.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/cuixin/cloud/zk"
+	"github.com/golang/glog"
+	zookeeper "github.com/samuel/go-zookeeper/zk"
+	"time"
+)
+
+func watchZK(existed bool, event zookeeper.Event) {
+	glog.Infof("Existed [%v], event [%v]", existed, event)
+}
+
+func InitZK(addrs []string, listenAddr string) error {
+	conn, err := zk.Connect(addrs, time.Second)
+	if err != nil {
+		return err
+	}
+	zk.Create(conn, "/MsgBusServers")
+	glog.Infof("Connect zk[%v] OK!", addrs)
+	createErr := zk.CreateTempW(conn, "/MsgBusServers", listenAddr, watchZK)
+	if createErr != nil {
+		return createErr
+	}
+	return nil
+}
