2007f4c6cbb83778fa4e0bca2ab2b729df86de1e
steven <steven.cuixin@gmail.com>
2014-06-13 14:49:18 +0800

    support msgbus to push message.

--- msgbus/handler.go
+++ msgbus/handler.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"github.com/golang/glog"
 )
 
 func MainHandle(msg []byte) {
+	uid := binary.LittleEndian.Uint64(msg[:8])
+	GUserMap.PushToComet(int64(uid), msg)
 	glog.Info(msg)
 }
 
