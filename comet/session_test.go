e3e26c02ab666cc244805a989f8a034b3ad8ad1c
steven <steven.cuixin@gmail.com>
2014-06-19 15:42:55 +0800

    add session test.

---
+++ comet/session_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cuixin/cloud/hlist"
+	"sync"
+	"testing"
+)
+
+func TestSession(t *testing.T) {
+	g := InitSessionList()
+	lens := 10
+	els := make([]*hlist.Element, 0, lens)
+	wg := sync.WaitGroup{}
+	wg.Add(lens)
+	for i := 0; i < lens; i++ {
+		go func(id int) {
+			s1 := NewSession(int64(id), "a", "1111", nil, nil)
+			e1 := g.AddSession(s1)
+			els = append(els, e1)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	wg.Add(lens)
+	for i := 0; i < lens; i++ {
+		go func(id int) {
+			g.RemoveSession(els[id])
+			fmt.Println("remove:", els[id])
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Printf("%#v\n", g.kv)
+}
