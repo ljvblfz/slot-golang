8d5c9370e72ec4a5fcbe90ccfdb8417f4cf289f8
yangying <yangying@blackcrystalinfo.com>
2014-08-20 18:20:27 +0800

    fix bug: msgbus can reconnect rmq twice when connection was closed

--- msgbus/rmq.go
+++ msgbus/rmq.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 
 	"cloud/hlist"
 
@@ -25,7 +26,7 @@ type Rmqs struct {
 	mu		sync.Mutex
 }
 
-func (this *Rmqs) Add(addr string, exchangeName string) {
+func (this *Rmqs) Add(addr string, exchangeName string) error {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 
@@ -33,7 +34,7 @@ func (this *Rmqs) Add(addr string, exchangeName string) {
 		s, _ := e.Value.(*rmqServer)
 		if s.addr == addr {
 			glog.Warningf("[rmq] repeat online server %s", addr)
-			return
+			return nil
 		}
 	}
 
@@ -41,14 +42,16 @@ func (this *Rmqs) Add(addr string, exchangeName string) {
 	conn, err := amqp.Dial(url)
 	if err != nil {
 		glog.Errorf("[rmq] dial to server %s failed: %v", addr, err)
-		return
+		return err
 	}
 	channel, err := conn.Channel()
 	if err != nil {
 		conn.Close()
 		glog.Errorf("[rmq] get channel from rabbitmq server %s failed: %v", addr, err)
-		return
+		return err
 	}
+	closeChan := make(chan *amqp.Error, 1)
+	conn.NotifyClose(closeChan)
 
 	err = channel.ExchangeDeclare(
 		exchangeName,
@@ -63,19 +66,45 @@ func (this *Rmqs) Add(addr string, exchangeName string) {
 		glog.Errorf("[rmq] declare exchange %s failed from rabbitmq server %s failed: %v", exchangeName, addr, err)
 		channel.Close()
 		conn.Close()
-		return
+		return err
 	}
 
-	this.servers.PushFront(&rmqServer{
+	s := &rmqServer{
 		conn: conn,
 		channel: channel,
 		addr: addr,
 		exchangeName: exchangeName,
-	})
+	}
+	this.servers.PushFront(s)
 
 	if this.curr == nil {
 		this.curr = this.servers.Front()
 	}
+
+	go this.listenClose(closeChan, s)
+
+	return nil
+}
+
+func (this *Rmqs) listenClose(c chan *amqp.Error, s *rmqServer) {
+	err := <-c
+	glog.Errorf("[rmq|close] close event on rmq server %v, error: %v", s, err)
+
+	addr := s.addr
+	exchangeName := s.exchangeName
+
+	this.Remove(s)
+
+	// retry twice connecting to rmq server
+	for i := 0; i < 2; i++ {
+		time.Sleep(5 * time.Second)
+		glog.Infof("[amq|reconnecting] reconnecting %d ...", i + 1)
+		if nil == this.Add(addr, exchangeName) {
+			glog.Infof("[amq|reconnecting] reconnect successful on %d times", i + 1)
+			break
+		}
+	}
+	glog.Errorf("[amq|reconnecting] reconnect failed")
 }
 
 // remove s but not close
@@ -120,7 +149,7 @@ func (this *Rmqs) Push(msg []byte, serviceId int) {
 				go func(server *rmqServer) {
 					this.Remove(server)
 					server.Close()
-					glog.Errorf("[rmq|close] close server %v on error: %v", server, err)
+					glog.Errorf("[rmq|close] close server %v when pushing msg, error: %v", server, err)
 				}(c)
 			}
 		}
