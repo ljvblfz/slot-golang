2612c9c2fdf82c1fc791010850102ac287ed5444
yangying <yangying@blackcrystalinfo.com>
2014-09-09 14:00:33 +0800

    add script for packing deployment zip package

---
+++ pack/pack.bash
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: $0 new-version"
+	exit
+fi
+
+Pwd=$(cd "$(dirname "$0")";pwd)
+Ver=$1
+BinDir=$Pwd/$Ver
+PackDir=(comet msgbus rmqkeeper)
+DirSize=${#PackDir[@]}
+GitBranch=master
+
+mkdir -p src
+cd src
+
+if [ -d cloud ]; then
+	echo "Pull repository"
+	cd cloud
+	git pull
+else
+	echo "Clone new repository"
+	git clone git@192.168.0.231:serverside/cloud.git
+	cd cloud
+fi
+
+[[ -d $BinDir ]] || mkdir $BinDir
+
+for((i=0;i<DirSize;i++))
+do
+	[[ -d $BinDir/${PackDir[i]} ]] || mkdir $BinDir/${PackDir[i]}
+	[[ -d $Pwd/${PackDir[i]} ]] || mkdir $Pwd/${PackDir[i]}
+done
+
+git checkout $GitBranch
+
+# modify version
+mkdir -p $Pwd/src/cloud/ver
+echo -e "package ver\n\nvar Version = \"$Ver\"" > $Pwd/src/cloud/ver/ver.go
+git add $Pwd/src/cloud/ver/ver.go
+git commit -m "modify version to $Ver"
+
+echo "Packing branch $GitBranch"
+
+echo "Type in log for v$Ver:"
+git tag -a socket-v$Ver
+git push
+
+for((i=0;i<DirSize;i++))
+do
+	echo "Building ${PackDir[i]} ..."
+	cd $Pwd/src/cloud/${PackDir[i]}
+	GOPATH=$Pwd:$GOPATH go build -a
+	#&& cp ${PackDir[i]} $BinDir/${PackDir[i]}
+done
+
+echo "Zipping $Ver"
+AllDir=""
+for((i=0;i<DirSize;i++))
+do
+	AllDir="$AllDir ${PackDir[i]}"
+done
+
+cd $PwdDir
+[[ -d pack ]] || mkdir pack
+zip -r pack/powersocket-v"$Ver"-`date +%Y%m%d`.zip $AllDir
+
