14655aa4044043380d865cfc62098069b5edc7b0
yangying <yangying@blackcrystalinfo.com>
2014-09-26 15:21:34 +0800

    modify dog shell for msgbus

--- msgbus/deploy.sh
+++ msgbus/deploy.sh
@@ -2,7 +2,6 @@
 
 for x in {13..13}
 do
+	scp msgbus root@192.168.2.$x:/root/bin/msgbus_update
 	ssh root@192.168.2.$x killall msgbus
-	scp msgbus root@192.168.2.$x:/root/bin/msgbus
-	ssh root@192.168.2.$x 'cd /root/bin && ./msgbus_run.sh'
 done
